ac21eb73ed074103401c7a5ccc371b236e015d26
Ben Cipollini <ben@learningequality.org>
2013-12-15 10:14:03 -0800

    Should fix #1167

--- kalite/updates/management/commands/videoscan.py
+++ kalite/updates/management/commands/videoscan.py
@@ -40,11 +40,11 @@ class Command(BaseCommand):
         videos_marked_at_all = set([video.youtube_id for video in VideoFile.objects.all()])
         videos_marked_as_in_progress = set([video.youtube_id for video in VideoFile.objects.filter(download_in_progress=True)])
         videos_marked_as_unstarted = set([video.youtube_id for video in VideoFile.objects.filter(percent_complete=0, download_in_progress=False)])
-        
+
         videos_flagged_for_download = set([video.youtube_id for video in VideoFile.objects.filter(flagged_for_download=True)])
         videos_in_filesystem = set([path.replace("\\", "/").split("/")[-1].split(".")[0] for path in files])
 
-        # Files that exist, but are not in the DB, should be assumed to be good videos, 
+        # Files that exist, but are not in the DB, should be assumed to be good videos,
         #   and just needing to be added to the DB.  Add them to the DB in this way,
         #   so that these files also trigger the update code below (and trigger cache invalidation)
         video_ids_needing_model_creation = list(videos_in_filesystem - videos_marked_at_all)
@@ -54,6 +54,8 @@ class Command(BaseCommand):
             VideoFile.objects.bulk_create([VideoFile(youtube_id=id, percent_complete=0, download_in_progress=False) for id in video_ids_needing_model_creation])
             self.stdout.write("Created %d VideoFile models (to mark them as complete, since the files exist)\n" % len(video_ids_needing_model_creation))
             touched_video_ids += [i18n.get_video_id(yid) or yid for yid in video_ids_needing_model_creation]
+            for video in video_ids_needing_model_creation:
+                caching.invalidate_on_video_update(sender=handle, instance=video)
 
         # Files that exist, are in the DB, but have percent_complete=0, download_in_progress=False
         #   These should be individually saved to be 100% complete, to trigger their availability (and cache invalidation)
@@ -61,10 +63,10 @@ class Command(BaseCommand):
         for chunk in break_into_chunks(videos_in_filesystem):
             video_files_needing_model_update = VideoFile.objects.filter(percent_complete=0, download_in_progress=False, youtube_id__in=chunk)
             count += video_files_needing_model_update.count()
-            for videofile in video_files_needing_model_update:
-                videofile.percent_complete = 100
-                videofile.flagged_for_download = False
-                videofile.save()
+            video_files_needing_model_update.update(percent_complete=100, flagged_for_download=False)
+            for video in video_files_needing_model_update:
+                caching.invalidate_on_video_update(sender=handle, instance=video)
+
         if count:
             self.stdout.write("Updated %d VideoFile models (to mark them as complete, since the files exist)\n" % count)
 
